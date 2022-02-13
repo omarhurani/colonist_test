7e8b38a75d6f6d5a5dcb3c8157fb6df703c93e56
omarhurani <omarhurani@gmail.com>
2022-02-13 21:19:40 +0200

    Make canvas fill screen with fixed aspectRatio

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,8 @@
 app.onInit = function(){
+
+    this.onResize()
+    window.addEventListener('resize', () => this.onResize())
+
     this.nodes.push(new Node(
         'red-box',
         { x : 100, y : 0 },
@@ -37,3 +41,28 @@ app.onUpdate = function(time){
         red.direction = Math.PI - red.direction
     }
 };
+
+app.onResize = function(){
+    {
+        let aspectRatio = 2;
+        let width, height
+        if(window.innerWidth / window.innerHeight > aspectRatio){
+            width = window.innerHeight * aspectRatio;
+            height = window.innerHeight;
+        } else{
+            width = window.innerWidth;
+            height = window.innerWidth / aspectRatio;
+        }
+        
+        this.context.mozImageSmoothingEnabled = false;
+        this.context.webkitImageSmoothingEnabled = false;
+        this.context.msImageSmoothingEnabled = false;
+        this.context.imageSmoothingEnabled = false;
+
+        // By changing the canvas.style size, the contents of the canvas are scaled to the new size
+        // This will make it such that absolute sizes become relative to the canvas.style size
+        this.canvas.style.width = `${width}px`;
+        this.canvas.style.height = `${height}px`;
+
+    }
+}
