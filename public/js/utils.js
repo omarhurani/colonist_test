e905c823cd99daadbfa1ae2bed8823487da7a7a0
omarhurani <omarhurani@gmail.com>
2022-02-17 23:17:33 +0200

    Add function to calculate circle to circle intersection

--- public/js/utils.js
+++ public/js/utils.js
@@ -37,3 +37,19 @@ function replaceFontSizeInFont(font, newFontSize){
 
     return newFont
 }
+
+function circleIntersectsCircle(circle1, circle2){
+    
+    const [x1, y1] = [circle1.x, circle1.y]
+    const [x2, y2] = [circle2.x, circle2.y]
+    const [distanceX, distanceY] = [x2 - x1, y2 - y1]
+
+    // Pythagorean theorem
+    const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY)
+
+    const [radius1, radius2] = [circle1.radius, circle2.radius]
+    const sumOfRadii = radius1 + radius2
+    const circlesIntersecting = distance < sumOfRadii
+
+    return circlesIntersecting
+}
