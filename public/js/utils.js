5b0f587f00a7ab0afc303855e2af0a668adfad3c
omarhurani <omarhurani@gmail.com>
2022-02-17 21:09:48 +0200

    Add clamp function

--- public/js/utils.js
+++ public/js/utils.js
@@ -9,3 +9,14 @@ class Keys{
 
     static SPACE = 'Space'
 }
+
+function clamp(value, min, max){
+    const valueLargerThanMax = value > max
+    const valueSmallerThanMin = value < min
+    if(valueLargerThanMax)
+        return max
+    else if(valueSmallerThanMin)
+        return min
+    else
+        return value
+}
